854cab37dbc87c3bcb238cb8075dc8cc27a73ee1
Deathkael83 <135058800+Deathkael83@users.noreply.github.com>
2025-11-24 01:46:59 +0100

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,5 @@
 const MODULE_ID = "wfrp4e-zero-wounds-prone";
-const LOCAL = MODULE_ID;
+const PREFIX = MODULE_ID;
 
 Hooks.once("init", function () {
 
@@ -8,7 +8,7 @@ Hooks.once("init", function () {
   /* --------------------------------------------- */
 
   game.settings.register(MODULE_ID, "headerGeneral", {
-    name: game.i18n.localize(`${LOCAL}.settings.headerGeneral`),
+    name: game.i18n.localize(`${PREFIX}.settings.headerGeneral`),
     scope: "world",
     config: true,
     type: String,
@@ -16,8 +16,8 @@ Hooks.once("init", function () {
   });
 
   game.settings.register(MODULE_ID, "enableModule", {
-    name: `${LOCAL}.settings.enableModule.name`,
-    hint: `${LOCAL}.settings.enableModule.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.enableModule.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.enableModule.hint`),
     scope: "world",
     config: true,
     type: Boolean,
@@ -29,7 +29,7 @@ Hooks.once("init", function () {
   /* --------------------------------------------- */
 
   game.settings.register(MODULE_ID, "headerPC", {
-    name: game.i18n.localize(`${LOCAL}.settings.headerPC`),
+    name: game.i18n.localize(`${PREFIX}.settings.headerPC`),
     scope: "world",
     config: true,
     type: String,
@@ -37,8 +37,8 @@ Hooks.once("init", function () {
   });
 
   game.settings.register(MODULE_ID, "enablePC", {
-    name: `${LOCAL}.settings.enablePC.name`,
-    hint: `${LOCAL}.settings.enablePC.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.enablePC.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.enablePC.hint`),
     scope: "world",
     config: true,
     type: Boolean,
@@ -46,22 +46,22 @@ Hooks.once("init", function () {
   });
 
   game.settings.register(MODULE_ID, "pcProneMode", {
-    name: `${LOCAL}.settings.pcProneMode.name`,
-    hint: `${LOCAL}.settings.pcProneMode.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcProneMode.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcProneMode.hint`),
     scope: "world",
     config: true,
     type: String,
     choices: {
-      disabled: `${LOCAL}.settings.mode.disabled`,
-      chat: `${LOCAL}.settings.mode.chat`,
-      auto: `${LOCAL}.settings.mode.auto`
+      disabled: game.i18n.localize(`${PREFIX}.settings.mode.disabled`),
+      chat: game.i18n.localize(`${PREFIX}.settings.mode.chat`),
+      auto: game.i18n.localize(`${PREFIX}.settings.mode.auto`)
     },
     default: "chat"
   });
 
   game.settings.register(MODULE_ID, "pcProneAutoNotify", {
-    name: `${LOCAL}.settings.pcProneAutoNotify.name`,
-    hint: `${LOCAL}.settings.pcProneAutoNotify.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcProneAutoNotify.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcProneAutoNotify.hint`),
     scope: "world",
     config: true,
     type: Boolean,
@@ -69,22 +69,22 @@ Hooks.once("init", function () {
   });
 
   game.settings.register(MODULE_ID, "pcUnconsciousMode", {
-    name: `${LOCAL}.settings.pcUnconsciousMode.name`,
-    hint: `${LOCAL}.settings.pcUnconsciousMode.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcUnconsciousMode.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcUnconsciousMode.hint`),
     scope: "world",
     config: true,
     type: String,
     choices: {
-      disabled: `${LOCAL}.settings.mode.disabled`,
-      chat: `${LOCAL}.settings.mode.chat`,
-      auto: `${LOCAL}.settings.mode.auto`
+      disabled: game.i18n.localize(`${PREFIX}.settings.mode.disabled`),
+      chat: game.i18n.localize(`${PREFIX}.settings.mode.chat`),
+      auto: game.i18n.localize(`${PREFIX}.settings.mode.auto`)
     },
     default: "chat"
   });
 
   game.settings.register(MODULE_ID, "pcUnconsciousAutoNotify", {
-    name: `${LOCAL}.settings.pcUnconsciousAutoNotify.name`,
-    hint: `${LOCAL}.settings.pcUnconsciousAutoNotify.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcUnconsciousAutoNotify.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcUnconsciousAutoNotify.hint`),
     scope: "world",
     config: true,
     type: Boolean,
@@ -92,23 +92,23 @@ Hooks.once("init", function () {
   });
 
   game.settings.register(MODULE_ID, "pcWakeMode", {
-    name: `${LOCAL}.settings.pcWakeMode.name`,
-    hint: `${LOCAL}.settings.pcWakeMode.hint`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcWakeMode.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcWakeMode.hint`),
     scope: "world",
     config: true,
     type: String,
     choices: {
-      disabled: `${LOCAL}.settings.mode.disabled`,
-      chat: `${LOCAL}.settings.mode.chat`,
-      auto: `${LOCAL}.settings.mode.auto`
+      disabled: game.i18n.localize(`${PREFIX}.settings.mode.disabled`),
+      chat: game.i18n.localize(`${PREFIX}.settings.mode.chat`),
+      auto: game.i18n.localize(`${PREFIX}.settings.mode.auto`)
     },
     default: "chat"
   });
 
   game.settings.register(MODULE_ID, "pcWakeAutoNotify", {
-    name: `${LOCAL}.settings.pcWakeAutoNotify.name`,
-    hint: `${LOCAL}.settings.pcWakeAutoNotify.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcWakeAutoNotify.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcWakeAutoNotify.hint`),
+    scope: "world",
     config: true,
     type: Boolean,
     default: true
@@ -119,7 +119,7 @@ Hooks.once("init", function () {
   /* --------------------------------------------- */
 
   game.settings.register(MODULE_ID, "headerNPC", {
-    name: game.i18n.localize(`${LOCAL}.settings.headerNPC`),
+    name: game.i18n.localize(`${PREFIX}.settings.headerNPC`),
     scope: "world",
     config: true,
     type: String,
@@ -127,78 +127,78 @@ Hooks.once("init", function () {
   });
 
   game.settings.register(MODULE_ID, "enableNPC", {
-    name: `${LOCAL}.settings.enableNPC.name`,
-    hint: `${LOCAL}.settings.enableNPC.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.enableNPC.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.enableNPC.hint`),
+    scope: "world",
     config: true,
     type: Boolean,
     default: true
   });
 
   game.settings.register(MODULE_ID, "npcProneMode", {
-    name: `${LOCAL}.settings.npcProneMode.name`,
-    hint: `${LOCAL}.settings.npcProneMode.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcProneMode.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcProneMode.hint`),
+    scope: "world",
     config: true,
     type: String,
     choices: {
-      disabled: `${LOCAL}.settings.mode.disabled`,
-      chat: `${LOCAL}.settings.mode.chat`,
-      auto: `${LOCAL}.settings.mode.auto`
+      disabled: game.i18n.localize(`${PREFIX}.settings.mode.disabled`),
+      chat: game.i18n.localize(`${PREFIX}.settings.mode.chat`),
+      auto: game.i18n.localize(`${PREFIX}.settings.mode.auto`)
     },
     default: "chat"
   });
 
   game.settings.register(MODULE_ID, "npcProneAutoNotify", {
-    name: `${LOCAL}.settings.npcProneAutoNotify.name`,
-    hint: `${LOCAL}.settings.npcProneAutoNotify.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcProneAutoNotify.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcProneAutoNotify.hint`),
+    scope: "world",
     config: true,
     type: Boolean,
     default: true
   });
 
   game.settings.register(MODULE_ID, "npcUnconsciousMode", {
-    name: `${LOCAL}.settings.npcUnconsciousMode.name`,
-    hint: `${LOCAL}.settings.npcUnconsciousMode.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcUnconsciousMode.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcUnconsciousMode.hint`),
+    scope: "world",
     config: true,
     type: String,
     choices: {
-      disabled: `${LOCAL}.settings.mode.disabled`,
-      chat: `${LOCAL}.settings.mode.chat`,
-      auto: `${LOCAL}.settings.mode.auto`
+      disabled: game.i18n.localize(`${PREFIX}.settings.mode.disabled`),
+      chat: game.i18n.localize(`${PREFIX}.settings.mode.chat`),
+      auto: game.i18n.localize(`${PREFIX}.settings.mode.auto`)
     },
     default: "chat"
   });
 
   game.settings.register(MODULE_ID, "npcUnconsciousAutoNotify", {
-    name: `${LOCAL}.settings.npcUnconsciousAutoNotify.name`,
-    hint: `${LOCAL}.settings.npcUnconsciousAutoNotify.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcUnconsciousAutoNotify.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcUnconsciousAutoNotify.hint`),
+    scope: "world",
     config: true,
     type: Boolean,
     default: true
   });
 
   game.settings.register(MODULE_ID, "npcWakeMode", {
-    name: `${LOCAL}.settings.npcWakeMode.name`,
-    hint: `${LOCAL}.settings.npcWakeMode.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcWakeMode.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcWakeMode.hint`),
+    scope: "world",
     config: true,
     type: String,
     choices: {
-      disabled: `${LOCAL}.settings.mode.disabled`,
-      chat: `${LOCAL}.settings.mode.chat`,
-      auto: `${LOCAL}.settings.mode.auto`
+      disabled: game.i18n.localize(`${PREFIX}.settings.mode.disabled`),
+      chat: game.i18n.localize(`${PREFIX}.settings.mode.chat`),
+      auto: game.i18n.localize(`${PREFIX}.settings.mode.auto`)
     },
     default: "chat"
   });
 
   game.settings.register(MODULE_ID, "npcWakeAutoNotify", {
-    name: `${LOCAL}.settings.npcWakeAutoNotify.name`,
-    hint: `${LOCAL}.settings.npcWakeAutoNotify.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcWakeAutoNotify.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcWakeAutoNotify.hint`),
+    scope: "world",
     config: true,
     type: Boolean,
     default: true
@@ -209,51 +209,51 @@ Hooks.once("init", function () {
   /* --------------------------------------------- */
 
   game.settings.register(MODULE_ID, "headerRecipients", {
-    name: game.i18n.localize(`${LOCAL}.settings.headerRecipients`),
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.headerRecipients`),
+    scope: "world",
     config: true,
     type: String,
     default: ""
   });
 
   game.settings.register(MODULE_ID, "pcRecipientsMain", {
-    name: `${LOCAL}.settings.pcRecipientsMain.name`,
-    hint: `${LOCAL}.settings.pcRecipientsMain.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcRecipientsMain.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcRecipientsMain.hint`),
+    scope: "world",
     config: true,
     type: String,
     choices: {
-      gmOnly: `${LOCAL}.settings.recipients.gmOnly`,
-      owners: `${LOCAL}.settings.recipients.owners`,
-      everyone: `${LOCAL}.settings.recipients.everyone`
+      gmOnly: game.i18n.localize(`${PREFIX}.settings.recipients.gmOnly`),
+      owners: game.i18n.localize(`${PREFIX}.settings.recipients.owners`),
+      everyone: game.i18n.localize(`${PREFIX}.settings.recipients.everyone`)
     },
     default: "gmOnly"
   });
 
   game.settings.register(MODULE_ID, "pcRecipientsWake", {
-    name: `${LOCAL}.settings.pcRecipientsWake.name`,
-    hint: `${LOCAL}.settings.pcRecipientsWake.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.pcRecipientsWake.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.pcRecipientsWake.hint`),
+    scope: "world",
     config: true,
     type: String,
     choices: {
-      gmOnly: `${LOCAL}.settings.recipients.gmOnly`,
-      owners: `${LOCAL}.settings.recipients.owners`,
-      everyone: `${LOCAL}.settings.recipients.everyone`
+      gmOnly: game.i18n.localize(`${PREFIX}.settings.recipients.gmOnly`),
+      owners: game.i18n.localize(`${PREFIX}.settings.recipients.owners`),
+      everyone: game.i18n.localize(`${PREFIX}.settings.recipients.everyone`)
     },
     default: "gmOnly"
   });
 
   game.settings.register(MODULE_ID, "npcRecipients", {
-    name: `${LOCAL}.settings.npcRecipients.name`,
-    hint: `${LOCAL}.settings.npcRecipients.hint`,
-    scope: "world`,
+    name: game.i18n.localize(`${PREFIX}.settings.npcRecipients.name`),
+    hint: game.i18n.localize(`${PREFIX}.settings.npcRecipients.hint`),
+    scope: "world",
     config: true,
     type: String,
     choices: {
-      gmOnly: `${LOCAL}.settings.recipients.gmOnly`,
-      owners: `${LOCAL}.settings.recipients.owners`,
-      everyone: `${LOCAL}.settings.recipients.everyone`
+      gmOnly: game.i18n.localize(`${PREFIX}.settings.recipients.gmOnly`),
+      owners: game.i18n.localize(`${PREFIX}.settings.recipients.owners`),
+      everyone: game.i18n.localize(`${PREFIX}.settings.recipients.everyone`)
     },
     default: "gmOnly"
   });
